474a734de9d54d81d1e18083cb3fe9bf569f679a
majdarov <majdarov@gmail.com>
2025-11-13 22:47:04 +0400

    Modified my-tests.sh

--- my-tests.sh
+++ my-tests.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/bash
 
-connections=$(nmcli c | grep vpn | cut -d ' ' -f 1)
+# connections=( nmcli c | grep vpn | cut -d ' ' -f 1 )
+connections=( nmcli c | cut -d ' ' -f 1 )
 
-arr_conn=()
-for conn in $connections
+# arr_conn=()
+# for conn in $connections
+# do
+#   arr_conn+=($conn)
+# done
+
+for i in "${connections}"
 do
-  arr_conn+=($conn)
+  echo "  $((i + 1))) ${connections[$i]}"
 done
 
-for i in ${!arr_conn[@]}
-do
-  echo "  $((i + 1))) ${arr_conn[$i]}"
-done
-
+exit 0
